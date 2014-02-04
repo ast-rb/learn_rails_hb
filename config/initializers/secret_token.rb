a488c15b6fd6b2db0902fb3b262a58c6563971d3
Astral <ravendv@list.ru>
2014-02-04 20:43:21 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hb::Application.config.secret_token = '52d1788ea2f4e60de58a9e1c1577a15fe0787541b8abacca54f7934f19a393fbc374e33a045f717ddee9b4d66fcef76907e17018e9fab1683d91c7e96eefb823'
